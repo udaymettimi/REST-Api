60aa955d9dd5f9126a360ab032333342910e30e8
udaymettimi <udaykiran990197@gmail.com>
2025-03-17 09:48:42 +0530

    msg

--- app.py
+++ app.py
@@ -1,45 +1,40 @@
-from flask import Flask,jsonify
+from flask import Flask, jsonify
 
 todo = Flask('__name__')
 
 students = [
-        {
-            'id':1,
-            'student_name': 'std1',
-            'age':21,
-            'email':'trs@gmail.com'
-        },
-        {
-            'id':2,
-            'student_name': 'std12',
-            'age': 22,
-            'email': 'smd@gmail.com'
-        },
-        {
-            'id':3,
-            'student_name': 'std3',
-            'age': 21,
-            'email': 'trs2@gmail.com'
-        }
+    {
+        'id': 1,
+        'student_name': 'std1',
+        'age': 20,
+        'email': 'higuys23@gmail.com'
+    },
+    {
+        'id': 2,
+        'student_name': 'std2',
+        'age': 23,
+        'email': 'hello3@gmail.com'
+    },
+    {
+        'id': 3,
+        'student_name': 'std3',
+        'age': 25,
+        'email': 'higuys2@gmail.com'
+    }
 ]
-@todo.route('/students-list')
+
+# Endpoint to get the full list of students
+@todo.route('/students_list')
 def students_list():
     return jsonify(students)
 
-@todo.route('/student/get/<int:id>',methods=['GET'])
-def student_id(id):
-    student=next((student for student in students if student['id']==id),None)
+# Endpoint to get a student by ID
+@todo.route('/student/get/<int:id>')
+def student_get_by_id(id):
+    for std in students:
+        if std['id'] == id:
+            return jsonify(std)
+    return "id not found", 404
 
-    if student is None:
-        return jsonify({'message':'Student not found'}),404
-
-    return jsonify(student)
-
-
-
-if __name__== "__main__":
-    todo.run(
-        host='127.0.0.1',
-        port=5000,
-        debug=True
-    )
+if __name__ == '__main__':
+    todo.run(debug=True)
